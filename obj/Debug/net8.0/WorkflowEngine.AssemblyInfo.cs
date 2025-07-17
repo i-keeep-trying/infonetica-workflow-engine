1a1b7bae03a6478383e10703c15db71cb9728f07
i-keeep-trying <soham.pal2005@gmail.com>
2025-07-18 01:49:59 +0530

    adding readme

--- obj/Debug/net8.0/WorkflowEngine.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorkflowEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkflowEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73e2b9e22a59bb38d4f855bd1d5a573eac0552c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkflowEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkflowEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
